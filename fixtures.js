3277806578dbe6cdd7c738de44d60a31c67ffc56
Thiago Lagden <lagden@gmail.com>
2018-06-19 12:33:51 -0300

    exemplo para armazenar as mensagens

---
+++ fixtures.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const dados = [
+	{
+		"_read": {
+			"950619": 1
+		},
+		"broker": 8,
+		"date": 1529352830878,
+		"from": 950619,
+		"id": "949a6ab7-65ed-40b1-8ad1-fcb9be0f372a",
+		"read": false,
+		"room": "37046_950619",
+		"text": "eu...",
+		"to": [
+			37046
+		],
+		"type": "message"
+	},
+	{
+		"_read": {
+			"950619": 1
+		},
+		"broker": 8,
+		"date": 1529352830877,
+		"from": 950619,
+		"id": "949a6ab7-65ed-40b1-8ad1-fcb9be0f372b",
+		"read": false,
+		"room": "37046_950619",
+		"text": "aqui...",
+		"to": [
+			37046
+		],
+		"type": "message"
+	},
+	{
+		"_read": {
+			"950619": 1
+		},
+		"broker": 8,
+		"date": 1529352830877,
+		"from": 950619,
+		"id": "949a6ab7-65ed-40b1-8ad1-fcb9be0f372c",
+		"read": false,
+		"room": "37046_950620",
+		"text": "outra room...",
+		"to": [
+			37046
+		],
+		"type": "message"
+	}
+]
+
+export default dados
